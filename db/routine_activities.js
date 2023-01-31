62520f311dc6b07e3cc97e5052d4727210cf9b97
AMT98 <aswinmalla12@gmail.com>
2023-01-30 20:44:42 -0700

    Create addActivityToRoutine function

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -5,7 +5,19 @@ async function addActivityToRoutine({
   activityId,
   count,
   duration,
-}) {}
+}) {
+  try {
+    const { rows: [activityroutine] } = await client.query(`
+    INSERT INTO routine_activites (routineId, activityId, count, duration)
+    VALUES ($1, $2, $3 , $4)
+    RETURNING *;
+    `,
+    [routineId, activityId, count, duration]
+    )
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 async function getRoutineActivityById(id) {}
 
