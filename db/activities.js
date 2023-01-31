f8a521844d96a2dbeb668355381f3c7fe031de42
AMT98 <aswinmalla12@gmail.com>
2023-01-30 20:09:41 -0700

    Create functions in activities.js

--- db/activities.js
+++ db/activities.js
@@ -1,26 +1,93 @@
-const client = require('./client');
- 
+const client = require("./client");
+
 // database functions
 async function createActivity({ name, description }) {
   // return the new activity
+  try {
+    const {
+      rows: [activity],
+    } = await client.query(
+      `
+    INSERT INTO acitivites(name , description)
+    VALUES($1,$2)
+    ON CONFLICT (name) DO NOTHING
+    RETURNING *;
+    `,
+      [name, description]
+    );
+    return activity;
+  } catch (error) {
+    throw error;
+  }
 }
 
 async function getAllActivities() {
   // select and return an array of all activities
+  try {
+    const { rows } = await client.query(`
+    SELECT id, name, description
+    FROM activities;
+    `);
+    return rows;
+  } catch (error) {
+    throw error;
+  }
 }
 
-async function getActivityById(id) {}
+async function getActivityById(id) {
+  try {
+    const {
+      rows: [activity],
+    } = await client.query(`
+    SELECT id, name, description
+    FROM activities
+    WHERE ID=${id}
+    `);
+    if (!activity) {
+      return null;
+    }
 
-async function getActivityByName(name) {}
+    return activity;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getActivityByName(name) {
+  try {
+    const {
+      rows: [activity],
+    } = await client.query(
+      `
+      SELECT *
+      FROM activities
+      WHERE name=$1;
+  `,
+      [name]
+    );
+
+    return activity;
+  } catch (error) {
+    throw error;
+  }
+}
 
 async function attachActivitiesToRoutines(routines) {
   // select and return an array of all activities
+  try {
+  } catch (error) {
+    throw error;
+  }
 }
 
 async function updateActivity({ id, ...fields }) {
   // don't try to update the id
   // do update the name and description
   // return the updated activity
+  try {
+  } catch (error) {
+    throw error;
+  }
 }
 
 module.exports = {
