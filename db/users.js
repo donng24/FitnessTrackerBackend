f466ffaf4b2e868800a6627fffe9c8f6b3037325
Don Nnguyen <donng24@gmail.com>
2023-01-30 21:31:35 -0600

    createUser

--- db/users.js
+++ db/users.js
@@ -1,14 +1,27 @@
 const client = require("./client");
 
+const bcrypt = require("bcrypt");
 // database functions
 
 // user functions
 async function createUser({ username, password }) {
-  
+    const SALT_COUNT = 10;
+    const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
+    const { rows: [user] } = await client.query(`
+    INSERT INTO users(username, password)
+    VALUES ($1, $2)
+    ON CONFLICT (username) DO NOTHING
+    RETURNING*, 
+    `, [username, hashedPassword]);
+
+    return user;
 }
 
 async function getUser({ username, password }) {
+  const user = await getUserByUserName(username);
+  const hashedPassword = user.password;
 
+  const isValid = await bcrypt.compare(password, hashedPassword)
 }
 
 async function getUserById(userId) {
--- tests/db/users.spec.js
+++ tests/db/users.spec.js
@@ -17,7 +17,7 @@ const { createFakeUser } = require("../helpers");
 
 describe("DB Users", () => {
   describe("createUser({ username, password })", () => {
-    xit("Creates and returns the user", async () => {
+    it("Creates and returns the user", async () => {
       const fakeUserData = {
         username: "Horace",
         password: faker.internet.password(),
@@ -26,7 +26,7 @@ describe("DB Users", () => {
       expect(user.username).toBe(fakeUserData.username);
     });
 
-    xit("Does NOT return the password", async () => {
+    it("Does NOT return the password", async () => {
       const fakeUserData = {
         username: faker.internet.userName(),
         password: faker.internet.password(),
