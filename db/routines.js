b02bc342be762a1bcba7a6ca3d3fc9d72e9db4cc
AMT98 <aswinmalla12@gmail.com>
2023-01-31 10:20:40 -0700

    Fixed error creating routine

--- db/routines.js
+++ db/routines.js
@@ -2,20 +2,20 @@ const client = require("./client");
 
 async function createRoutine({ creatorId, isPublic, name, goal }) {
   try {
-    const {
-      rows: [routine],
-    } = await client.query(
-      `
-    INSERT INTO routines("creatorId", "isPublic", name, goal)
-    VALUES($1, $2, $3, $4)
-    RETURNING *;
-    `[creatorId, isPublic, name, goal]
+    const {rows: [routine],} = await client.query(`
+    INSERT INTO routines ("creatorId", "isPublic", name, goal)
+    VALUES ($1, $2, $3, $4)
+    ON CONFLICT (name) DO NOTHING
+    RETURNING *
+    `,
+      [creatorId, isPublic, name, goal]
     );
     return routine;
   } catch (error) {
     console.log(error);
   }
 }
+
 
 async function getRoutineById(id) {}
 
