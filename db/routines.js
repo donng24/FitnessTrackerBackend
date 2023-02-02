f2a5521697a762a8593b2e45ec243cb1b73f65ff
AMT98 <aswinmalla12@gmail.com>
2023-02-02 01:23:14 -0700

    destroy routes also passing all tests

--- db/routines.js
+++ db/routines.js
@@ -128,16 +128,14 @@ async function destroyRoutine(id) {
     await client.query(
       `
     DELETE FROM routine_activities
-    WHERE "routineId"=$1;
-`,
+    WHERE "routineId"=$1;`,
       [id]
     );
 
     await client.query(
       `
     DELETE FROM routines
-    WHERE id=$1;
-`,
+    WHERE id=$1;`,
       [id]
     );
   } catch (error) {
