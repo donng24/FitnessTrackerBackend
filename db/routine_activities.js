1ef3a4230bb15d7e63fa157d6fd186eb0feb2520
Don Nguyen <donng24@gmail.com>
2023-02-01 20:40:11 -0600

    finished routine_activities

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -10,6 +10,7 @@ async function addActivityToRoutine({
     const { rows: [activityroutine] } = await client.query(`
     INSERT INTO routine_activities ("routineId", "activityId", count, duration)
     VALUES ($1, $2, $3 , $4)
+    ON CONFLICT ("routineId", "activityId") DO NOTHING 
     RETURNING *;
     `,
     [routineId, activityId, count, duration]
@@ -20,15 +21,63 @@ async function addActivityToRoutine({
   }
 }
 
-async function getRoutineActivityById(id) {}
+async function getRoutineActivityById(id) {
+  try {
+    const { rows: [activityroutine] } = await client.query( `SELECT * FROM routine_activities WHERE id = $1` ,
+      [id]
+    )
+  return activityroutine
+} catch (error) {
+  console.log(error);
+  }
+}
+  
+async function getRoutineActivitiesByRoutine({ id }) {
+  try {
+    const { rows } = await client.query(`SELECT * FROM routine_activities WHERE "routineId"= $1`, 
+    [id])
+    return rows
+  }catch (error){
+    console.log(error)
+  }
+}
 
-async function getRoutineActivitiesByRoutine({ id }) {}
+async function updateRoutineActivity({ id, ...fields }) {
+  try {
+    const {count, duration} = fields;
+    const { rows: [activityroutine] } = await client.query(
+      `UPDATE routine_activities SET count=$1, duration=$2 WHERE id=$3 RETURNING *`,
+      [count, duration, id]
+    );
+    return activityroutine;
+  }catch (error) {
+    console.log(error)
+  }
+}
 
-async function updateRoutineActivity({ id, ...fields }) {}
+async function destroyRoutineActivity(id) {
+  try {
+    const { rows: [activityroutine] } = await client.query(`DELETE FROM routine_activities WHERE id=$1 RETURNING *`,
+    [id]
+    );
+    return activityroutine;
+  }catch (error) {
+    console.log(error);
+  }
+}
 
-async function destroyRoutineActivity(id) {}
+async function canEditRoutineActivity(routineActivityId, userId) {
+  try {
+    const { rows: [routineActivity] } = await client.query(`SELECT * FROM routine_activities WHERE id = $1 AND user_id = $2` , 
+    [routineActivityId, userId]);
 
-async function canEditRoutineActivity(routineActivityId, userId) {}
+  return !!routineActivity;
+
+} catch (error) {
+  console.log(error);
+  }
+}
+
 
 module.exports = {
   getRoutineActivityById,
