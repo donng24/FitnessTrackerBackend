446bcf972a4647ee7d0476c544de6c36aa25f984
Don Nnguyen <donng24@gmail.com>
2023-02-06 21:30:58 -0600

    routine_activities api, worked on DELETE

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -1,8 +1,44 @@
 const express = require('express');
-const router = express.Router();
-
+const id = require('faker/lib/locales/id_ID');
+const routineActivitiesRouter = express.Router();
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = process.env;
+const {
+  getRoutineActivityById,
+  addActivityToRoutine,
+  getRoutineActivitiesByRoutine,
+  updateRoutineActivity,
+  destroyRoutineActivity,
+  canEditRoutineActivity,
+  } = require("../db");
 // PATCH /api/routine_activities/:routineActivityId
+routineActivitiesRouter.patch('/:routineActivityId', (req, res, next) => {
 
+}
+)
 // DELETE /api/routine_activities/:routineActivityId
+routineActivitiesRouter.delete('/:routineActivityId', async (req, res, next) => {
+    try {
+      const token = req.header('Authorization').replace('Bearer ', '');
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const routineActivityId = req.params.routineActivityId;
+      const userId = decoded.id;
 
-module.exports = router;
+      const routineActivity = await getRoutineActivityById(routineActivityId);
+        if (routineActivity.id !== userId) {
+        return res.status(401).send({ 
+            activityId: activityId
+            count: count
+            duration: duration
+            id: id
+            routineId: routineId
+         });
+      }
+  
+      await destroyRoutineActivity(routineActivityId);
+      res.status(204).send;
+    } catch (error) {
+      next(error);
+    }
+  });
+module.exports = routineActivitiesRouter;
