5a23ba34166f29af310e6120a9ca0a927129b77c
Don Nnguyen <donng24@gmail.com>
2023-02-08 20:32:18 -0600

    routineActivities

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -1,8 +1,6 @@
 const express = require('express');
-const id = require('faker/lib/locales/id_ID');
 const routineActivitiesRouter = express.Router();
 const jwt = require('jsonwebtoken')
-const { JWT_SECRET } = process.env;
 const {
   getRoutineActivityById,
   destroyRoutineActivity,
@@ -14,27 +12,24 @@ routineActivitiesRouter.patch('/:routineActivityId', (req, res, next) => {
 )
 // DELETE /api/routine_activities/:routineActivityId
 routineActivitiesRouter.delete('/:routineActivityId', async (req, res, next) => {
-    try {
-      const token = req.header('Authorization').replace('Bearer ', '');
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      const routineActivityId = req.params.routineActivityId;
-      const userId = decoded.id;
+  try {
+    const token = req.header('Authorization').replace('Bearer ', '');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const routineActivityId = req.params.routineActivityId;
+    const userId = decoded.id;
 
-      const routineActivity = await getRoutineActivityById(routineActivityId);
-        if (routineActivity.id !== userId) {
-        return res.status(401).send({ 
-            activityId: activityId,
-            count: count,
-            duration: duration,
-            id: id,
-            routineId: routineId
-         });
-      }
-  
-      await destroyRoutineActivity(routineActivityId);
-      res.status(204).send;
-    } catch (error) {
-      next(error);
+    const routineActivity = await getRoutineActivityById(routineActivityId);
+    if (!routineActivity) {
+      return res.status(404).send({ error: "Routine activity not found" });
     }
-  });
+    if (routineActivity.userId !== userId) {
+      return res.status(403).send();
+    }
+
+    await destroyRoutineActivity(routineActivityId);
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
+});
 module.exports = routineActivitiesRouter;
