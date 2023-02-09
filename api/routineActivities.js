7120ad0c2c294d1de79adbe96a876526f49fb995
Don Nnguyen <donng24@gmail.com>
2023-02-08 21:26:50 -0600

    tweaks

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -11,31 +11,25 @@ const {
 // PATCH /api/routine_activities/:routineActivityId
 routineActivitiesRouter.patch("/:routineActivityId", (req, res, next) => {});
 // DELETE /api/routine_activities/:routineActivityId
-routineActivitiesRouter.delete(
-  "/:routineActivityId",
-  async (req, res, next) => {
-    try {
-      const token = req.header("Authorization").replace("Bearer ", "");
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      const routineActivityId = req.params.routineActivityId;
-      const userId = decoded.id;
+routineActivitiesRouter.delete('/:routineActivityId', async (req, res, next) => {
+  try {
+    // Verify the authenticity of the user
+    const token = req.header('Authorization').replace('Bearer ', '');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const routineActivityId = req.params.routineActivityId;
+    const userId = decoded.id;
 
-      const routineActivity = await getRoutineActivityById(routineActivityId);
-      if (routineActivity.id !== userId) {
-        return res.status(401).send({
-          activityId: activityId,
-          count: count,
-          duration: duration,
-          id: id,
-          routineId: routineId,
-        });
-      }
-
-      await destroyRoutineActivity(routineActivityId);
-      res.status(204).send;
-    } catch (error) {
-      next(error);
+    // Check if the user is the owner of the routine activity
+    const routineActivity = await getRoutineActivityById(routineActivityId);
+    if (routineActivity.userId !== userId) {
+      return res.status(401).send({ error: "You are not authorized to perform this action" });
     }
+
+    // Delete the routine activity
+    await destroyRoutineActivity(routineActivityId);
+    res.status(204).send();
+  } catch (error) {
+    next(error);
   }
-);
+});
 module.exports = routineActivitiesRouter;
