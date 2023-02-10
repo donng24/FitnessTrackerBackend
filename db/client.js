6e9251c6f5684bf8f283d8865b4ad9edea5e4f2a
AMT98 <aswinmalla12@gmail.com>
2023-02-09 20:04:31 -0700

    Fixed error

--- db/client.js
+++ db/client.js
@@ -1,10 +1,14 @@
-const { Pool } = require('pg');
-
-const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/fitness-dev';
-
+const { Pool } = require("pg");
+require("dotenv").config();
+const connectionString =
+  process.env.DATABASE_URL || "https://localhost:5432/fitness-dev";
+console.log(connectionString, "HEREE");
 const client = new Pool({
   connectionString,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : undefined,
 });
 
 module.exports = client;
