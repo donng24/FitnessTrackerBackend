3267a4c0104386dd83f5f0c6fb1e36012085e2f0
Alexandra Labaisse <beatsnmelody1@gmail.com>
2023-02-09 18:44:43 -0600

    patch done again

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -8,7 +8,7 @@ const {
   destroyRoutineActivity,
   canEditRoutineActivity,
 } = require("../db");
-// PATCH /api/routine_activities/:routineActivityId
+// PATCH /api/routine_activities/:routineActivityId :)
 routineActivitiesRouter.patch("/:routineActivityId", async (req, res, next) => { 
   try {
   const { routineActivityId } = req.params;
@@ -35,25 +35,31 @@ routineActivitiesRouter.patch("/:routineActivityId", async (req, res, next) => {
 }
 });
 // DELETE /api/routine_activities/:routineActivityId
-routineActivitiesRouter.delete('/:routineActivityId', async (req, res, next) => {
-  try {
-    // Verify the authenticity of the user
-    const token = req.header('Authorization').replace('Bearer ', '');
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const routineActivityId = req.params.routineActivityId;
-    const userId = decoded.id;
+routineActivitiesRouter.delete(
+  "/:routineActivityId",
+  async (req, res, next) => {
+    try {
+      const token = req.header("Authorization").replace("Bearer ", "");
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const routineActivityId = req.params.routineActivityId;
+      const userId = decoded.id;
 
-    // Check if the user is the owner of the routine activity
-    const routineActivity = await getRoutineActivityById(routineActivityId);
-    if (routineActivity.userId !== userId) {
-      return res.status(401).send({ error: "You are not authorized to perform this action" });
+      const routineActivity = await getRoutineActivityById(routineActivityId);
+      if (routineActivity.id !== userId) {
+        return res.status(401).send({
+          activityId: activityId,
+          count: count,
+          duration: duration,
+          id: id,
+          routineId: routineId,
+        });
+      }
+
+      await destroyRoutineActivity(routineActivityId);
+      res.status(204).send;
+    } catch (error) {
+      next(error);
     }
-
-    // Delete the routine activity
-    await destroyRoutineActivity(routineActivityId);
-    res.status(204).send();
-  } catch (error) {
-    next(error);
   }
-});
+);
 module.exports = routineActivitiesRouter;
