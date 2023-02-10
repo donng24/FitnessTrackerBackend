15480895d34a4e4aa6ad7cb3683c3901582243bf
AMT98 <aswinmalla12@gmail.com>
2023-02-09 18:49:14 -0700

    34/35 tests passing

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -8,35 +8,41 @@ const {
   destroyRoutineActivity,
   canEditRoutineActivity,
   getRoutineById,
+  updateRoutineActivity,
 } = require("../db");
 const { UnauthorizedDeleteError } = require("../errors");
 const { requireUser } = require("./utils");
 // PATCH /api/routine_activities/:routineActivityId :)
-routineActivitiesRouter.patch("/:routineActivityId", async (req, res, next) => {
-  try {
-    const { routineActivityId } = req.params;
-    const currentUser = req.user;
+routineActivitiesRouter.patch(
+  "/:routineActivityId",
+  requireUser,
+  async (req, res, next) => {
+    console.log(req.params);
+    try {
+      const { routineActivityId } = req.params;
 
-    const routineActivity = await getRoutineActivityById(routineActivityId);
-
-    if (routineActivity && routineActivity.creatorId === req.user.id) {
-      const updatedRoutineActivity = await updateRoutineActivity({
-        id: routineId,
-        ...req.body,
-      });
-      res.send(updatedRoutineActivity);
-    } else if (routineActivity.createdBy !== currentUser.id) {
-      res.status(403);
-      next({
-        error: "Error",
-        message: `User ${req.user.username} is not allowed to update Every day`,
-        name: "Error",
-      });
+      const routineActivity = await getRoutineActivityById(routineActivityId);
+      if (routineActivity.createdBy !== req.user.id) {
+        res.status(403);
+        next({
+          error: "Error",
+          message: `User ${req.user.username} is not allowed to update In the evening`,
+          name: "Error",
+        });
+      } else {
+        const updatedRoutineActivity = await updateRoutineActivity({
+          id: routineActivityId,
+          ...req.body,
+        });
+        if (updatedRoutineActivity) {
+          res.send(updatedRoutineActivity);
+        }
+      }
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error);
   }
-});
+);
 // DELETE /api/routine_activities/:routineActivityId
 routineActivitiesRouter.delete(
   "/:routineActivityId",
@@ -50,7 +56,6 @@ routineActivitiesRouter.delete(
       const deleteRoutineActivity = await destroyRoutineActivity(id);
 
       const routine = await getRoutineById(routineActivity.routineId);
-      console.log(routine, "here", req.user);
       if (routine.creatorId === req.user.id) {
         res.send(deleteRoutineActivity);
       } else {
